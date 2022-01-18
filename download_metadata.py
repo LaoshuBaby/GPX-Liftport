deca5490ed26c8ec7088fb74664dd93a802f2a76
LaoshuBaby <keaitianxinmail@qq.com>
2022-01-18 22:27:16 +0800

    Update download_metadata.py

--- download_metadata.py
+++ download_metadata.py
@@ -5,11 +5,21 @@ URL_PATTERN={
     "download_metadata":"https://{server}.openstreetmap.org/api/0.6/gpx/{id}/details",
 }
 
-def call_func(server_name:str,gpx_id:int):
+header={
+    "Authorization":"Basic {auth}",
+}
+
+username="喵耳引力波"
+password="0"
+
+def call_func(server_name:str,gpx_id:int,mode:str):
     print("server_name="+server_name,"gpx_id="+str(gpx_id))
     url=URL_PATTERN["download_metadata"].format(server=server_name)
     url=url.format(id=gpx_id)
-    body = requests.get(url)
+    if mode=="simple":
+        body = requests.get(url)
+    else:
+        body = requests.get(url,auth=(username,password)) #不愧是你啊，Copilot，我还打算手写header替换和base64库引入来着
     print(body.text)
 
 def init():
@@ -19,6 +29,8 @@ def init():
         server_name="www"
     else:
         server_name="master.apis.dev" # "api06.dev"
-    call_func(server_name,gpx_id)
+    # username=input("请输入您的用户名：")
+    password=input("请输入您的密码：")
+    call_func(server_name,gpx_id,"auth")
 
 init()
